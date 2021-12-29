6bf3e4489ed0f7c8af2c50f72fcc11b6edebd9c1
Dominique Luna <dluna132@gmail.com>
2021-12-29 16:19:53 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.24

---
+++ src/TransformerCircuits.jl
@@ -0,0 +1,5 @@
+module TransformerCircuits
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TransformerCircuits
+using Test
+
+@testset "TransformerCircuits.jl" begin
+    # Write your tests here.
+end
