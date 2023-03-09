8cc25ea435bb741bd87911496c720699cf42f8c1
Dominique Luna <dluna132@gmail.com>
2023-03-09 11:55:24 -0600

    rm func

--- src/TransformerCircuits.jl
+++ src/TransformerCircuits.jl
@@ -8,7 +8,6 @@ const A3{T} = Array{T,3} where {T<:Union{Float32,Float16}}
 
 # https://transformer-circuits.pub/2022/solu
 solu(x) = x .* softmax(x)
-roundto64(x) = x % 64 != 0 ? x + 64 - x % 64 : x
 
 include("attention.jl")
 include("blocks.jl")
