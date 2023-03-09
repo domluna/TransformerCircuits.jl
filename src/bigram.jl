7c9d117c49fdbf0071a5632235d734dc5bdee781
Dominique Luna <dluna132@gmail.com>
2023-03-09 12:17:40 -0600

    fix

--- src/bigram.jl
+++ src/bigram.jl
@@ -18,10 +18,10 @@ struct BiGramWithPosition{E}
 end
 Flux.@functor BiGramWithPosition
 
-BiGram(vocabsize::Int, contextsize::Int) =
+BiGramWithPosition(vocabsize::Int, contextsize::Int) =
     BiGramWithPosition(Flux.Embedding(vocabsize, vocabsize), Flux.Embedding(contextsize, vocabsize))
 
-function (b::BiGramWithPosition)(x::Matrix{Int64}, idx::size(x, 1))
+function (b::BiGramWithPosition)(x::Matrix{Int64}, idx::Int=size(x, 1))
     tokemb = b.token_embed(x)
     posemb = b.position_embed(1:idx)
     return tokemb .+ posemb
